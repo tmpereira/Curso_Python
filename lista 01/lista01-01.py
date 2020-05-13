bb40a40fa03ab001eb137df2b0ddd347e60f08a4
tmpereira <t.pereira@unifesp.br>
2020-05-13 15:15:56 -0300

    adicionei uns bla bla

--- lista 01/lista01-01.py
+++ lista 01/lista01-01.py
@@ -3,7 +3,6 @@ Crie um script em Python que solicite o nome, altura e peso
 de uma pessoa e mostre a seguinte mensagem: “João tem
 90 kilos e altura de 1.78 e portanto o IMC é de 28.4”
 
-blah balgh bsf´ghbsngrbç\gb vcbngndbvn
 '''
 
 nome = input('digite o nome do usuario')
