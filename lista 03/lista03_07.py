f67f6a94e700c6bb5c9edab0507d63934add3f8c
tmpereira <t.pereira@unifesp.br>
2020-05-21 10:40:06 -0300

    pasta teste

--- lista 03/lista03_07.py
+++ lista 03/lista03_07.py
@@ -1,5 +1,5 @@
 
-for i in range(1,5):
+for i in range(1,6):
     for j in range(i,i*i+1,i):
         print(str(j) +' ',end='')
     print('')
--- teste/teste2.py
+++
@@ -1,5 +0,0 @@
-num = 123456
-while(num > 0):
-    i = num%10
-    print(i)
-    num = num//10
