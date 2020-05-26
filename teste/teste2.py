eb813c6fcc430e1aebd5b7b2685f28b3770c5a1a
tmpereira <t.pereira@unifesp.br>
2020-05-26 14:16:41 -0300

    pasta teste 2

--- teste/teste2.py
+++ teste/teste2.py
@@ -1,17 +1,7 @@
-'''
-matrix =[[1,2,3],[4,5,6],[3,6,7]]
-matrix3x = [[0,0,0],[0,0,0],[0,0,0]]
-for i in range(0,3):
-    for j in range(0,3):
-        matrix3x[i][j] = 3*matrix[i][j]
-        print(str(matrix3x[i][j]) + ' ',end='')
-    print('')
-'''
+contato ={
+    'nome':'thiago',
+    'sobrenome':'pereira',
+    'cpf':123456789
+}
 
-import numpy as np
-
-m =[[1,2,3],[4,5,6],[3,6,7]]
-mndarray = np.array(m)
-m[0,0]
-m[0][0]
-
+print(contato.items())
