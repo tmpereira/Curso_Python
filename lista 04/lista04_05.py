7aa50f369431184f5aebb54329afa6688ad1b64e
tmpereira <t.pereira@unifesp.br>
2020-05-28 08:18:29 -0300

    pasta lista 04

--- lista 04/lista04_05.py
+++ lista 04/lista04_05.py
@@ -1,3 +1,14 @@
+'''
+Crie um programa que leia nome, sexo, peso e altura de
+várias pessoas. guarde os dados de cada pessoa num
+dicionário individual e acrescente o IMC da pessoa.
+Organize todos os dicionários em uma lista. No final
+mostre
+a. Quantas pessoas foram cadastradas
+b. Qual é o peso médio das pessoas
+c. Qual é a altura média das pessoas
+d. Qual é IMC médio das pessoas
+'''
 npessoas = int(input('digite a quantidade de pesssoas que deseja cadastrar: '))
 lista =[]
 pessoa ={}
